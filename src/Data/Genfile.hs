7ee8cac7c957555a995cbc8ce0d43b235783b699
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-05-19 20:48:09 +0200

    Reformatting

--- src/Data/Genfile.hs
+++ src/Data/Genfile.hs
@@ -1,30 +1,32 @@
-{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+
 module Data.Genfile
-    ( GenFile(..)
-    , GenDirTree
+  ( GenFile(..)
+  , GenDirTree
+  , renderDirTree
+  , genFile
+  , inDir
+  , rootDirTree
+  ) where
 
-    , genFile
-    , inDir
-    , rootDirTree
-    , renderDirTree
-    ) where
+import Control.Monad.Writer
+import Path
 
-import           Control.Monad.Writer
-import           Data.List            (nub)
-import           Path
-import           Path.IO              (ensureDir)
-
-import           Data.Genfile.Types
-import           Data.Genfile.Render
+import Data.Genfile.Render
+import Data.Genfile.Types
 
 -- | Generate a file in a writer monad.
-genFile :: MonadWriter (GenDirTree rel str) m => Path rel File -> str -> m ()
+genFile
+  :: MonadWriter (GenDirTree rel str) m
+  => Path rel File -> str -> m ()
 genFile path contents = tell [GenFile path contents]
 
 -- | Generate all the files in the given writer action under the given
 -- directory.
-inDir :: MonadWriter (GenDirTree Rel str) m => Path Rel Dir -> m a -> m a
+inDir
+  :: MonadWriter (GenDirTree Rel str) m
+  => Path Rel Dir -> m a -> m a
 inDir dir = censor $ map (\(GenFile p c) -> GenFile (dir </> p) c)
 
 -- | Root a tree of files relative to a relative directory.
